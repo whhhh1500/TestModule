46ffa3ecad9f5a4068eaae548f32610981da5daf
whhhh1500 <cc15009080204@163.com>
2020-04-30 16:01:53 +0800

    HttpServer

---
+++ netty-all/src/main/java/com/cc1500/nettyall/httpserver/HttpServer.java
@@ -0,0 +1,46 @@
+package com.cc1500.nettyall.httpserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.*;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class HttpServer {
+    public static void main(String[] args)  throws  Exception{
+        new HttpServer().run();
+    }
+
+    public  void run() throws  Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup worker = new NioEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap();
+        try {
+
+            b.group(bossGroup, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+
+                            ChannelPipeline pi=socketChannel.pipeline();
+                            pi.addLast("decoder", new HttpRequestDecoder());
+                            pi.addLast("encoder", new HttpResponseEncoder());
+                            pi.addLast("deflater", new HttpContentCompressor());
+                            pi.addLast(new HttpServerHandler());
+                        }
+                    });
+            ChannelFuture cf=b.bind(8899).sync();
+            cf.channel().closeFuture().sync();
+
+        }finally {
+            bossGroup.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+
+    }
+}

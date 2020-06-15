f9789faa1b9eea06d038b3aee315b2e6a2f8761d
whhhh1500 <cc15009080204@163.com>
2020-06-15 08:38:24 +0800

    no message

--- netty-all/src/main/java/com/cc1500/io/Nio.java
+++ netty-all/src/main/java/com/cc1500/io/Nio.java
@@ -14,15 +14,16 @@ import java.util.Set;
 public class Nio {
         public static void main(String[] args) throws IOException {
             Selector serverSelector = Selector.open();
-            Selector clientSelector = Selector.open();
+            Selector workSelector = Selector.open();
 
             new Thread(() -> {
                 try {
                     // 对应IO编程中服务端启动
-                    ServerSocketChannel listenerChannel = ServerSocketChannel.open();
-                    listenerChannel.socket().bind(new InetSocketAddress(8000));
-                    listenerChannel.configureBlocking(false);
-                    listenerChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
+                    ServerSocketChannel server = ServerSocketChannel.open();
+                    server.socket().bind(new InetSocketAddress(8000));
+                    server.configureBlocking(false);
+
+                    server.register(serverSelector, SelectionKey.OP_ACCEPT);
 
                     while (true) {
                         // 监测是否有新的连接，这里的1指的是阻塞的时间为1ms
@@ -38,7 +39,8 @@ public class Nio {
                                         // (1) 每来一个新连接，不需要创建一个线程，而是直接注册到clientSelector
                                         SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
                                         clientChannel.configureBlocking(false);
-                                        clientChannel.register(clientSelector, SelectionKey.OP_READ);
+                                        System.out.println(clientChannel.getRemoteAddress());
+                                        clientChannel.register(workSelector, SelectionKey.OP_READ);
                                     } finally {
                                         keyIterator.remove();
                                     }
@@ -57,8 +59,8 @@ public class Nio {
                 try {
                     while (true) {
                         // (2) 批量轮询是否有哪些连接有数据可读，这里的1指的是阻塞的时间为1ms
-                        if (clientSelector.select(1) > 0) {
-                            Set<SelectionKey> set = clientSelector.selectedKeys();
+                        if (workSelector.select(1) > 0) {
+                            Set<SelectionKey> set = workSelector.selectedKeys();
                             Iterator<SelectionKey> keyIterator = set.iterator();
 
                             while (keyIterator.hasNext()) {
--- springl/src/main/java/com/cc1500/test/test.java
+++ springl/src/main/java/com/cc1500/test/test.java
@@ -1,4 +1,37 @@
 package com.cc1500.test;
 
+import lombok.SneakyThrows;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class test {
+    public static void main(String[] args) {
+        Thread t1=    new ThreadTest();
+        t1.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(t1.isInterrupted());
+        t1.interrupt();
+       // System.out.println(t1.isInterrupted());
+    }
+
+static class ThreadTest extends  Thread{
+
+        @Override
+        public void run(){
+            boolean flg=true;
+            while(flg){
+               System.out.println(" Interrupted   "+flg);
+                if(currentThread().isInterrupted())
+                   flg=false;
+               
+
+            }
+        }
+
+}
 }
